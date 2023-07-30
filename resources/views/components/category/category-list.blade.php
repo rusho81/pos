6451e1bc78f9f478e33ddbf79345d43e40b552e7
Quazi Tariqul Alam <rushokazi@gmail.com>
2023-07-30 06:14:17 +0600

    initial

---
+++ resources/views/components/category/category-list.blade.php
@@ -0,0 +1,65 @@
+<div class="container-fluid">
+    <div class="row">
+    <div class="col-md-12 col-sm-12 col-lg-12">
+        <div class="card px-5 py-5">
+            <div class="row justify-content-between ">
+                <div class="align-items-center col">
+                    <h4>Category</h4>
+                </div>
+                <div class="align-items-center col">
+                    <button data-bs-toggle="modal" data-bs-target="#create-modal" class="float-end btn m-0 btn-sm bg-gradient-primary">Create</button>
+                </div>
+            </div>
+            <hr class="bg-dark "/>
+            <table class="table" id="tableData">
+                <thead>
+                <tr class="bg-light">
+                    <th>No</th>
+                    <th>Category</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody id="tableList">
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
+
+<script>
+   getList();
+   async function getList() {
+    showLoader();
+    let res = await axios.get("/list-category");
+    hideLoader();
+
+    let tableList = $('#tableList')
+    let tableData = $('#tableData')
+
+    tableData.DataTable().destroy();
+    tableList.empty();
+
+    res.data.forEach((item, index) => {
+        let row=`<tr>
+                    <td>${index+1}</td>
+                    <td>${item['name']}</td>
+                    <td>
+                        <button class ="btn btn-sm btn-outline-success">Edit</button>
+                        <button class ="btn btn-sm btn-outline-danger">Delete</button>
+                    </td>
+            
+                </tr>`
+        tableList.append(row)
+    })
+    
+     new DataTable('#tableData', {
+        order:[[0,'desc']],
+        lengthMenu:[2,4,10]
+     });
+
+    }
+
+    
+</script>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\TokenVarificationMiddleware;
+use Illuminate\Support\Facades\Route;
+
+
+//API Route
+Route::post('/user-registration', [UserController::class, 'UserRegistration']);
+Route::post('/user-login', [UserController::class, 'UserLogin']);
+Route::post('/send-otp', [UserController::class, 'SendOTPCode']);
+Route::post('/verify-otp', [UserController::class, 'VerifyOTP']);
+Route::post('/reset-password', [UserController::class, 'ResetPassword'])
+->middleware([TokenVarificationMiddleware::class]);
+
+Route::get('/user-profile', [UserController::class, 'UserProfile'])->middleware([TokenVarificationMiddleware::class]);
+Route::post('/user-update', [UserController::class, 'UpdateProfile'])->middleware([TokenVarificationMiddleware::class]);
+
+//logout
+Route::get('/logout',[UserController::class, 'UserLogout']);
+
+
+//Page Route
+Route::get('/userLogin',[UserController::class, 'LoginPage']);
+Route::get('/userRegistration',[UserController::class, 'RegistrationPage']);
+Route::get('/sendOtp',[UserController::class, 'SendOtpPage']);
+Route::get('/verifyOtp',[UserController::class, 'VerifyOtpPage']);
+Route::get('/resetPassword',[UserController::class, 'ResetPasswordPage'])->middleware([TokenVarificationMiddleware::class]);
+Route::get('/dashboard',[DashboardController::class, 'DashboardPage'])->middleware([TokenVarificationMiddleware::class]);
+Route::get('/userProfile',[UserController::class, 'ProfilePage'])->middleware([TokenVarificationMiddleware::class]);
+Route::get('/categoryPage',[CategoryController::class, 'CategoryPage'])->middleware([TokenVarificationMiddleware::class]);
+
+
+//Category API
+Route::post('/create-category',[CategoryController::class, 'CategoryCreate'])->middleware([TokenVarificationMiddleware::class]);
+Route::post('/delete-category',[CategoryController::class, 'CategoryDelete'])->middleware([TokenVarificationMiddleware::class]);
+Route::get('/list-category',[CategoryController::class, 'CategoryList'])->middleware([TokenVarificationMiddleware::class]);
+Route::post('/update-category',[CategoryController::class, 'CategoryUpdate'])->middleware([TokenVarificationMiddleware::class]);
